4c1492afe40c62ecf772a300f1d9184be53cca68
[am_6733] <[anton6733@gmail.com]>
2023-04-28 16:51:20 +0300

    later

--- notepad/noteRemover.py
+++ notepad/noteRemover.py
@@ -1,1 +1,25 @@
-import sqlite3
+
+
+from sqlite3 import Connection
+from CONSTANTS import BASEPATH
+
+from interface import db_note_remover
+from noteSelector import Note_selector
+
+class NoteRemover(db_note_remover):
+    def __init__(self,base:Connection):
+        self.__base=base
+    
+    def remove(self):
+        selector = Note_selector(self.__base)
+        return self.base
+
+
+def main():
+    a = Connection(BASEPATH)
+    b = NoteRemover(a)
+    
+
+
+if __name__=="__main__":
+    main()
